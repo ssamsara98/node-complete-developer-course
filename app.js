e2854cba29cc5a58c6102bed3b3334d50dfe7cb1
Sulthon Abdul Malik <sulthon.almalik@gmail.com>
2020-04-22 03:11:12 +0700

    Making HTTP Requests

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-console.log("Starting");
+const request = require("request");
 
-setTimeout(() => {
-  console.log("2 Second Timer");
-}, 2000);
+const url =
+  // "https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/37.8267,-122.4233";
+  "http://api.weatherstack.com/current?access_key=17522fcc4e88343d792ab6a5a3ad7e01&query=Sukabumi";
 
-setTimeout(() => {
-  console.log("0 Second Timer");
-}, 0);
-
-console.log("Stopping");
+request({ url: url }, (error, response) => {
+  const data = JSON.parse(response.body);
+  console.log(data.current);
+});
