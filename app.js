5260291548d99adfe616f26641534725c9d5c98d
Sulthon Abdul Malik <sulthon.almalik@gmail.com>
2020-04-21 19:31:38 +0700

    Getting Input from Users

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const chalk = require("chalk");
 const getNotes = require("./notes.js");
 
-const msg = getNotes();
-console.log(msg);
+const command = process.argv[2];
 
-const greenMsg = chalk.green.inverse.bold("Success!");
-console.log(greenMsg);
+console.log(process.argv);
 
-// install nodemon gobally
+if (command === "add") {
+  console.log("Adding note!");
+} else if (command === "remove") {
+  console.log("Removing note!");
+}
