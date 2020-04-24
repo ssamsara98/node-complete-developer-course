cb0ebd3c5e68c387727fe24ca386a71d77d0957d
Sulthon Abdul Malik <sulthon.almalik@gmail.com>
2020-04-24 13:58:53 +0700

    Data Validation and Sanitization - II

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -34,38 +34,40 @@ const User = mongoose.model("User", {
   },
 });
 
-const me = new User({
-  name: "   Andrew  ",
-  email: "MYEMAIL@MEAD.IO   ",
-});
+// const me = new User({
+//   name: "   Andrew  ",
+//   email: "MYEMAIL@MEAD.IO   ",
+// });
 
-me.save()
-  .then(() => {
-    console.log(me);
-  })
-  .catch((error) => {
-    console.log("Error!", error);
-  });
+// me.save()
+//   .then(() => {
+//     console.log(me);
+//   })
+//   .catch((error) => {
+//     console.log("Error!", error);
+//   });
 
 const Task = mongoose.model("Task", {
   description: {
     type: String,
+    required: true,
+    trim: true,
   },
   completed: {
     type: Boolean,
+    default: false,
   },
 });
 
-// const task = new Task({
-//   description: "Learn the Mongoose library",
-//   completed: false,
-// });
+const task = new Task({
+  description: "  Eat lunch",
+});
 
-// task
-//   .save()
-//   .then(() => {
-//     console.log(task);
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   });
+task
+  .save()
+  .then(() => {
+    console.log(task);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
