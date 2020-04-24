fb5de7f4f5b279187c3bc598eb1af678bb357dc1
Sulthon Abdul Malik <sulthon.almalik@gmail.com>
2020-04-24 13:48:01 +0700

    Mongoose Model

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -14,15 +14,38 @@ const User = mongoose.model("User", {
   },
 });
 
-const me = new User({
-  name: "Andrew",
-  age: 28,
+// const me = new User({
+//   name: "Andrew",
+//   age: 28,
+// });
+
+// me.save()
+//   .then(() => {
+//     console.log(me);
+//   })
+//   .catch((error) => {
+//     console.log("Error!", error);
+//   });
+
+const Task = mongoose.model("Task", {
+  description: {
+    type: String,
+  },
+  completed: {
+    type: Boolean,
+  },
 });
 
-me.save()
+const task = new Task({
+  description: "Learn the Mongoose library",
+  completed: false,
+});
+
+task
+  .save()
   .then(() => {
-    console.log(me);
+    console.log(task);
   })
   .catch((error) => {
-    console.log("Error!", error);
+    console.log(error);
   });
