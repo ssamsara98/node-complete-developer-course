aed4f63bcef496493f542a690ec9d54465c1920a
Sulthon Abdul Malik <sulthon.almalik@gmail.com>
2020-04-26 09:48:08 +0700

    Working with Timestamps

--- src/index.js
+++ src/index.js
@@ -33,10 +33,9 @@ const main = async () => {
   // const task = await Task.findById('5c2e505a3253e18a43e612e6')
   // await task.populate('owner').execPopulate()
   // console.log(task.owner)
-
-  const user = await User.findById("5ea2c341a007e7279cc6442a");
-  await user.populate("tasks").execPopulate();
-  console.log(user.tasks);
+  // const user = await User.findById('5c2e4dcb5eac678a23725b5b')
+  // await user.populate('tasks').execPopulate()
+  // console.log(user.tasks)
 };
 
 main();
