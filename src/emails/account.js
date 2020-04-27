1dd81f61511b6436d442ea02b9a279dab46f9b48
Sulthon Abdul Malik <sulthon.almalik@gmail.com>
2020-04-28 04:40:24 +0700

    Sending Welcome and Cancelation Emails

--- src/emails/account.js
+++ src/emails/account.js
@@ -5,9 +5,25 @@ const sendgridAPIKey =
 
 sgMail.setApiKey(sendgridAPIKey);
 
-sgMail.send({
-  to: "sulthon.almalik@gmail.com",
-  from: "sulthon.almalik@gmail.com",
-  subject: "This is my first creation!",
-  text: "I hope this one actually get to you.",
-});
+const sendWelcomeEmail = (email, name) => {
+  sgMail.send({
+    to: email,
+    from: "andrew@mead.io",
+    subject: "Thanks for joining in!",
+    text: `Welcome to the app, ${name}. Let me know how you get along with the app.`,
+  });
+};
+
+const sendCancelationEmail = (email, name) => {
+  sgMail.send({
+    to: email,
+    from: "andrew@mead.io",
+    subject: "Sorry to see you go!",
+    text: `Goodbye, ${name}. I hope to see you back sometime soon.`,
+  });
+};
+
+module.exports = {
+  sendWelcomeEmail,
+  sendCancelationEmail,
+};
