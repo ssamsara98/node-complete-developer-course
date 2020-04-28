a09e184fd9ec4e832326de7335c25b3456c7e863
Sulthon Abdul Malik <sulthon.almalik@gmail.com>
2020-04-28 17:25:30 +0700

    Deployment

--- src/app.js
+++ src/app.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV == "development") {
+  console.log("Development");
+  require("dotenv").config();
+}
 const path = require("path");
 const express = require("express");
 const hbs = require("hbs");
@@ -5,6 +9,7 @@ const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, "../public");
@@ -99,6 +104,6 @@ app.get("*", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Server is up on port 3000.");
+app.listen(port, () => {
+  console.log("Server is up on port " + port);
 });
