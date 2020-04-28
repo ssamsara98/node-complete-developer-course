31231b2bdd7bc15204a0b21927d4897e080dd802
Sulthon Abdul Malik <sulthon.almalik@gmail.com>
2020-04-28 21:07:42 +0700

    Testing an Express Application

---
+++ tests/user.test.js
@@ -0,0 +1,13 @@
+const request = require("supertest");
+const app = require("../src/app");
+
+test("Should signup a new user", async () => {
+  await request(app)
+    .post("/users")
+    .send({
+      name: "Andrew",
+      email: "samara@example.com",
+      password: "MyPass777!",
+    })
+    .expect(201);
+});
